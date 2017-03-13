c1de1c5ff931af93764e59132f715409b2ac2ca8
lduchosal <luc+github@2113.ch>
2017-03-13 13:45:30 +0100

    Version 2.0.4

--- doc/html/search/functions_6.js
+++ doc/html/search/functions_6.js
@@ -7,11 +7,11 @@ var searchData=
   ['touint',['ToUint',['../class_system_1_1_net_1_1_i_p_network.html#a4c9f4b27587d2cbe5e1ae7779212081a',1,'System::Net::IPNetwork']]],
   ['tryguesscidr',['TryGuessCidr',['../class_system_1_1_net_1_1_i_p_network.html#ae34614011cfca42104fa174838719755',1,'System::Net::IPNetwork']]],
   ['tryparse',['TryParse',['../class_system_1_1_net_1_1_i_p_network.html#a285fc05c46662e8e40dae6da4bf9e674',1,'System.Net.IPNetwork.TryParse(string ipaddress, string netmask, out IPNetwork ipnetwork)'],['../class_system_1_1_net_1_1_i_p_network.html#aec9eb3f2578c39c443b05ff03cc73347',1,'System.Net.IPNetwork.TryParse(string ipaddress, byte cidr, out IPNetwork ipnetwork)'],['../class_system_1_1_net_1_1_i_p_network.html#a2ecd2b8c75708cfe9529b8e9a11d6af9',1,'System.Net.IPNetwork.TryParse(string network, out IPNetwork ipnetwork)'],['../class_system_1_1_net_1_1_i_p_network.html#a1ef894da8be8506a2d0f07fac6a99fc6',1,'System.Net.IPNetwork.TryParse(IPAddress ipaddress, IPAddress netmask, out IPNetwork ipnetwork)']]],
-  ['tryparsecidr',['TryParseCidr',['../class_system_1_1_net_1_1_i_p_network.html#a6641a5efbc16b8fa30a1e4341ac1d3e8',1,'System::Net::IPNetwork']]],
+  ['tryparsecidr',['TryParseCidr',['../class_system_1_1_net_1_1_i_p_network.html#a54d74a4927f48e2df0560985a43cb025',1,'System::Net::IPNetwork']]],
   ['trysubnet',['TrySubnet',['../class_system_1_1_net_1_1_i_p_network.html#a4dbc2e9e4fa066942ebfa0b125e05b03',1,'System::Net::IPNetwork']]],
   ['trysupernet',['TrySupernet',['../class_system_1_1_net_1_1_i_p_network.html#af36d7ce0c405b22396a8dfc9a6d19e47',1,'System.Net.IPNetwork.TrySupernet(IPNetwork network1, IPNetwork network2, out IPNetwork supernet)'],['../class_system_1_1_net_1_1_i_p_network.html#a0dda64a72396a2eb7ffc359eb6243861',1,'System.Net.IPNetwork.TrySupernet(IPNetwork[] ipnetworks, out IPNetwork[] supernet)']]],
-  ['trytobiginteger',['TryToBigInteger',['../class_system_1_1_net_1_1_i_p_network.html#a2f45c9a2c236d6c99b6854936d14f232',1,'System::Net::IPNetwork']]],
-  ['trytocidr',['TryToCidr',['../class_system_1_1_net_1_1_i_p_network.html#a7d18beafd51ff0df31bf6f6f53e94ecd',1,'System::Net::IPNetwork']]],
+  ['trytobiginteger',['TryToBigInteger',['../class_system_1_1_net_1_1_i_p_network.html#a97c7f7065162a6ce03a0079286ce511e',1,'System::Net::IPNetwork']]],
+  ['trytocidr',['TryToCidr',['../class_system_1_1_net_1_1_i_p_network.html#a7352ee3ce1ef5785f5f732b6cfef71aa',1,'System::Net::IPNetwork']]],
   ['trytonetmask',['TryToNetmask',['../class_system_1_1_net_1_1_i_p_network.html#af6b2f2f649fa6006fe5af14b40ddd9dc',1,'System::Net::IPNetwork']]],
-  ['trytouint',['TryToUint',['../class_system_1_1_net_1_1_i_p_network.html#a45eff77752daa17b28e1980913040ddc',1,'System::Net::IPNetwork']]]
+  ['trytouint',['TryToUint',['../class_system_1_1_net_1_1_i_p_network.html#a75723db0ac5eef98d835941efad82286',1,'System::Net::IPNetwork']]]
 ];
